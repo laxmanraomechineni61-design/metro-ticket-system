06b80bda012b8fcb3db9195e108a4b51c2905120
Laxmanraomechineni <laxmanraomechineni61@gmail.com>
2026-01-18 15:37:21 -0600

    metro-ticket-system

---
+++ metro.py
@@ -0,0 +1,9 @@
+stations = {
+    "Kukatpally": 10,
+    "Dr BR Ambedkar": 15,
+    "Ameerpet": 20,
+    "SR Nagar": 25
+}
+
+print("--- Metro Ticket Counter ---")
+print("Stations: Kukatpally, Dr BR Ambedkar, Ameerpet, SR Nagar")
